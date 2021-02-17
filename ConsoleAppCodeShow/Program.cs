bea6bfa539a81924308125f80f4b2f62b9a94ec5
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2021-02-17 09:14:29 +0100

    Add project files.

---
+++ ConsoleAppCodeShow/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleAppCodeShow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Example()
+        {
+
+        }
+    }
+}
