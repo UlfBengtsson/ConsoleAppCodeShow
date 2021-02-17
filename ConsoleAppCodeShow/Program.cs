833d90dfabcafe383f7183a19fdffe45a5f2f126
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2021-02-17 10:43:20 +0100

    break time

--- ConsoleAppCodeShow/Program.cs
+++ ConsoleAppCodeShow/Program.cs
@@ -4,14 +4,29 @@ namespace ConsoleAppCodeShow
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//always starts here
         {
             Console.WriteLine("Hello World!");
+            Example2();
         }
 
-        static void Example()
+        static void Example1()
         {
+            string bear = Console.ReadLine();
 
+            Console.WriteLine(bear == "Bamse" ? "Thunder honey" : "Honey");
         }
-    }
-}
+
+        static void Example2()
+        {
+            Console.Write("Count down to zero from: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while (number >= 0)
+            {
+                Console.WriteLine("Number is: " + number--);
+            }
+        }
+
+    }//end of class program
+}//end of namespace
